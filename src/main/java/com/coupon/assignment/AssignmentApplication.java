d41d0e17505468f45c620d8f4471b333083e71b4
DESKTOP-LJQ78DH\의석 <dkdlelxoa@naver.com>
2025-07-27 18:23:14 +0900

    chore: 카프카 및 스케줄러 배치 설정

--- src/main/java/com/coupon/assignment/AssignmentApplication.java
+++ src/main/java/com/coupon/assignment/AssignmentApplication.java
@@ -1,9 +1,17 @@
 package com.coupon.assignment;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableKafka
+@EnableAsync
+@EnableScheduling
+@EnableBatchProcessing
 public class AssignmentApplication {
 
 	public static void main(String[] args) {
